d90316045cec950009b1306076f6870507980134
rltjqdl1138 <rltjqdl1138@naver.com>
2019-03-04 21:50:24 +0900

    Save results to mySQL

--- cstockx.js
+++ cstockx.js
@@ -1,6 +1,5 @@
+var mysql = require('mysql')
 var https = require('https')
-
-
 /*
  *  @variable	{Array}	brands	- 
  *    @variable	  {Object}
@@ -11,13 +10,61 @@ var https = require('https')
  *  @TODO
  *	Add other brands and models
 **/
+var mysql      = require('mysql');
+// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
+var connection = mysql.createConnection({
+  host		: '18.224.180.235',
+  user		: 'root',
+  port		: '3306',
+  password	: '1138877',
+  database	: 'buffedDB'
+});
+  
+connection.connect();
+console.log("connect to DB")
+
+
 var brands = [ {
 	name: 'adidas',
 	models:	['yeezy', 'ultra%20boost', 'nmd', 'iniki', 'other'] }
 ]
+function addToDB(product, callback){
 
+	product.name=product.name.replace(/\'/gi,"")
+	product.shoe=product.shoe.replace(/\'/gi,"")
+	product.shortDescription=product.shortDescription.replace(/\'/gi,"")
+	product.title=product.title.replace(/\'/gi,"")
+	var str = "INSERT INTO Sneakers VALUES('"
+			+product.brand+"','"
+			+product.category+"','"
+			+product.colorway+"','"
+			+product.name+"','"
+			+product.releaseDate+"',"
+			+product.releaseTime+","
+			+product.retailPrice+",'"
+			+product.shoe+"','"
+			+product.shortDescription+"','"
+			+product.title+"','"
+			+product.urlKey+"','"
+			+product.media.imageUrl+"','"
+			+product.id+"');"
+	connection.query(str, function (error, results, fields) {
+		if (error){
+			console.log("\n\n\n");
+			console.log(str)
+			console.log(error)
+			console.log(product)
+			console.log("\n\n\n");
+		}
+		else{
+			console.log("[success] "+product.title)
+		}
+	})
+	if(callback != null)
+		callback()
+}
 /*
- *  @function	getCallUsingBrand
+ *  @function	getCall
  *
  *  @param	{Object}	brand	- 
  *		{String}	model	-
@@ -74,7 +121,7 @@ function getCall( brand, model, year, page ) {
 	 *    If there is over 1000 items, they are ignored.
 	 *    So in this case, detail API Calls.
 	 *	1. one Brand name => many Model names
-	 *	  etc) Adidas => [ Adidas Yeezy, Adidas NMD, Adidas Iniki, ... ]
+	 * 	  etc) Adidas => [ Adidas Yeezy, Adidas NMD, Adidas Iniki, ... ]
 	 *	2. one Model name => many release years
 	 *	  etc) Adidas Yeezy => [ Adidas Yeezy 2002, ..., Adidas Yeezy 2019 ]
 	 *
@@ -87,7 +134,7 @@ function getCall( brand, model, year, page ) {
 		res.on('data', (data)=>{ merged += data })
 		res.on('end', ()=>{
 			var dat = JSON.parse(merged)
-			
+
 			//
 			// Detail API Call
 			//
@@ -106,21 +153,35 @@ function getCall( brand, model, year, page ) {
 				console.log("["+brand.name+" "+model+" "+year+"]\tEmpty!!")
 			}else if( dat.Pagination.nextPage != null ){
 				//console.log("["+brand.name+" "+model+" "+year+"]\t"+dat.Pagination.page+"/"+dat.Pagination.lastPage+"\t"+dat.Products.length+"items")
-				//
-				// [ INSERT DB ]
-				//
+				/*
+				 *  [ INSERT DB ]
+				 *  variable "dat.Products" is Array
+				 *  there is too many informations to write here
+				 */
+				console.log(dat.Pagination.page)
+				for(var i=0; i<dat.Products.length; i++){
+					//console.log(dat.Products[i].title)
+					addToDB(dat.Products[i],null)
+				}
+	
 				getCall( brand, model, year, Number(dat.Pagination.page)+1 )
 			} else{
+				/*
+				 *  [ INSERT DB ]
+				 */
+				for(var i=0; i<dat.Products.length; i++){
+					//console.log(dat.Products[i].title)
+					addToDB(dat.Products[i],null)
+				}
 				console.log("["+brand.name+" "+model+" "+year+"]\tDone!\t"+dat.Pagination.total+" items")
-				//
-				// [ INSERT DB ]
-				//
+				
 			}
 		})
 	})
 	getReq.end()
 	getReq.on('error', (err)=>{
 		console.log("Error: ", err)
+		connection.end();
 	})
 }
 
