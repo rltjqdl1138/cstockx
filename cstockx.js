ef4f6e0b9f508c71a60b1735aa306bda88a22d48
rltjqdl1138 <rltjqdl1138@naver.com>
2019-03-06 01:10:27 +0900

    Add all brand names

--- cstockx.js
+++ cstockx.js
@@ -1,5 +1,21 @@
 var mysql = require('mysql')
 var https = require('https')
+
+
+
+// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
+var connection = mysql.createConnection({
+  host		: '18.224.180.235',
+  user		: 'root',
+  port		: '3306',
+  password	: '1138877',
+  database	: 'buffedDB'
+});
+
+connection.connect();
+console.log("connect to DB")
+
+
 /*
  *  @variable	{Array}	brands	- 
  *    @variable	  {Object}
@@ -10,24 +26,37 @@ var https = require('https')
  *  @TODO
  *	Add other brands and models
 **/
-var mysql      = require('mysql');
-// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
-var connection = mysql.createConnection({
-  host		: '18.224.180.235',
-  user		: 'root',
-  port		: '3306',
-  password	: '1138877',
-  database	: 'buffedDB'
-});
-  
-connection.connect();
-console.log("connect to DB")
-
-
-var brands = [ {
-	name: 'adidas',
-	models:	['yeezy', 'ultra%20boost', 'nmd', 'iniki', 'other'] }
+var brands = [
+		{	name: 'adidas',
+			models:	['yeezy', 'ultra%20boost', 'nmd', 'iniki', 'other'] },
+		{	name: 'air%20jordan',
+			models: [ 'packs', 'spizike',
+					'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine','ten',
+					'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty',
+					'twenty-one', 'twenty-two', 'twenty-three', 'twenty-eight', 'twenty-nine', 'thirty',
+					'thirty-one', 'thirty-two', 'thirty-three', 'other']},
+		{ 	name: 'nike',
+			models: ['foamposite', 'kd', 'kobe', 'lebron', 'air%20force', 'air%20max', 'nike%20basketball', 'nike%20sb', 'other']},
+		{	name: 'asics',
+			models: ['asics'] },
+		{	name: 'diadora',
+			models: ['diadora']},
+		{	name: 'li%20ning',
+			models: ['li%20ning']},
+		{	name: 'louis%20vuitton',
+			models: ['louis%20vuitton']},
+		{	name: 'new%20balance',
+			models: ['new%20balance']},
+		{	name: 'puma',
+			models: ['puma']},
+		{	name: 'reebok',
+			models: ['reebok']},
+		{	name: 'saucony',
+			models: ['saucony']},
+		{	name: 'under%20armour',
+			models: ['under%20armour']}
 ]
+
 function addToDB(product, callback){
 
 	product.name=product.name.replace(/\'/gi,"")
@@ -150,7 +179,7 @@ function getCall( brand, model, year, page ) {
 					console.log("Warning!: ["+brand.name+" "+model+" "+year+"] is too many!")
 				}
 			}else if(dat.Pagination.lastPage == 0){
-				console.log("["+brand.name+" "+model+" "+year+"]\tEmpty!!")
+				//console.log("["+brand.name+" "+model+" "+year+"]\tEmpty!!")
 			}else if( dat.Pagination.nextPage != null ){
 				//console.log("["+brand.name+" "+model+" "+year+"]\t"+dat.Pagination.page+"/"+dat.Pagination.lastPage+"\t"+dat.Products.length+"items")
 				/*
@@ -158,7 +187,7 @@ function getCall( brand, model, year, page ) {
 				 *  variable "dat.Products" is Array
 				 *  there is too many informations to write here
 				 */
-				console.log(dat.Pagination.page)
+				//console.log(dat.Pagination.page)
 				for(var i=0; i<dat.Products.length; i++){
 					//console.log(dat.Products[i].title)
 					addToDB(dat.Products[i],null)
@@ -185,7 +214,50 @@ function getCall( brand, model, year, page ) {
 	})
 }
 
+function apiTest( brand, model ) {
+	var merged = ''
+	if( !brand || !brand.name ){
+		console.log( "brand name is empty" )
+		return
+	}
+	var options = {
+		host: 'stockx.com',
+		port: 443,
+		path: '/api/browse?productCategory=sneakers&sort=release_date&order=DESC',
+		method: 'GET'
+	}
+
+	if( model )
+		options.path += '&_tags='+model+','+brand.name
+	else
+		options.path += '&_tags='+brand.name
+	
+	var getReq = https.request(options, (res)=>{
+		res.on('data', (data)=>{ merged += data })
+		res.on('end', ()=>{
+			var dat = JSON.parse(merged)
+				if( model == null ){
+					brand.models.forEach( (e)=>{
+						apiTest( brand, e, null, null )
+					})
+				} else{
+					console.log("["+brand.name+"] "+model+":\t"+dat.Pagination.total)
+				}
+		})
+	})
+	getReq.end()
+	getReq.on('error', (err)=>{
+		console.log("Error: ", err)
+	})
+}
+
+
 //
 // Call the function for test..
 //
-getCall( brands[0] )
+//getCall( brands[0] )
+
+
+brands.forEach( (e)=>{
+	getCall(e)
+})
